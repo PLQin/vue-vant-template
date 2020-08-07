76a713b7a1cff60cf0f380e32c0cdea0d9387f1b
qingguipeng <773561801@qq.com>
2020-08-07 18:59:14 +0800

    Enables the service to start up properly

--- src/main.js
+++ src/main.js
@@ -1,19 +1,16 @@
 import Vue from 'vue'
-
-// 用于控制浏览器前进后退 使用keep-alive
-// import Navigation from 'vue-navigation'
-
 import App from './App.vue'
 import store from './store'
 import router from './router'
+
+// Vant
 import Vant from 'vant'
 import 'vant/lib/index.css'
+Vue.use(Vant)
 
 // 安装基础组件
 import BaseComponent from './base'
 Vue.use(BaseComponent, {})
-Vue.use(Vant)
-// Vue.prototype.$toast
 
 // 业务自定义组件
 import '@/components/index.js'
@@ -28,19 +25,13 @@ Vue.prototype.$moment = dayjs
 import VueClipboard from 'vue-clipboard2'
 Vue.use(VueClipboard)
 
-Vue.config.productionTip = false
-
-// 用于控制浏览器前进后退缓存
-// Vue.use(Navigation, {
-//   router,
-//   store
-// })
-
 // 开发环境下面使用vConsole进行调试
 if (process.env.NODE_ENV === 'development') {
   const VConsole = require('vconsole')
   new VConsole()
 }
+
+Vue.config.productionTip = false
 
 new Vue({
   router,
