d5e22b001135c12f9c74c05115e571ad7431a583
qingguipeng <773561801@qq.com>
2020-08-11 18:05:33 +0800

    Problem with page animation flashing up and down when routing is switched

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 
 <template>
   <div id="app">
-    <transition :name="transitionName">
+    <transition :name="transitionName" mode="out-in">
       <router-view class="router" />
     </transition>
   </div>
@@ -25,22 +25,4 @@ export default {
 }
 </script>
 
-<style lang="less">
-#app {
-  height: 100vh;
-  font-family: PingFangSC-Regular;
-  background-color: #f1f3f5;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
-
-/** 解决路由切换动画上下闪动问题 */
-.router {
-  position: absolute;
-  top: safe-area-inset-top;
-  top: env(safe-area-inset-top);
-  width: 100%;
-  transition: all 0.377s ease;
-  backface-visibility: hidden;
-}
-</style>
+<style lang="less" src='./app.less'></style>
