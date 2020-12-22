f12326a8e3fe5e4b4e71cd4d12374a05d8cb30b0
raymond <599964870@qq.com>
2020-12-22 11:43:38 +0800

    Update apo and language

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,8 @@
 import { get as getLanguage } from '@/utils/language.js'
 import axios from 'axios'
 import Vue from 'vue'
-import { Toast } from 'vant'
+import { Toast, Dialog } from 'vant'
+import store from '@/store/index.js'
 
 // 这样的话就可以使用 this.$axios 方法
 axios.defaults.baseURL = process.env.NODE_ENV === 'production' ? process.env.VUE_APP_BASE_API : '/'
@@ -17,7 +18,7 @@ const service = axios.create({
   //   'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'
   // },
 
-  timeout: 5000 // request timeout
+  timeout: 10000 // request timeout
 })
 Vue.prototype.$axios = axios
 
@@ -30,7 +31,7 @@ const defaultParams = {
 service.interceptors.request.use(
   config => {
     // 在请求发出之前进行一些操作
-    // config.headers['x-access-appid'] = 'ty9fd2848a039abbbb'
+    config.headers['x-access-login_token'] = localStorage.getItem('login_token')
 
     console.log(`--------> http ajax request`, config)
     config.data = Object.assign({}, defaultParams, config.data)
@@ -59,17 +60,27 @@ service.interceptors.response.use(
   response => {
     const res = response.data
 
-    // if the custom code is not 200, it is judged as an error.
     // 判断字符串而不是数值, 因为code可能是字母加数字组合
-    if (String(res.code) !== '200') {
-      Toast(res.msg || '服务器异常，请稍后再试')
-      return Promise.reject(res.message || 'Error')
-    } else {
-      return res
+    // 移动端（客户端）的code判断负责，不同场景即便code值相同但提示用户的message未必相同，因此
+    // 具体的message提示由接口调用出进行处理
+    switch (String(res.code)) {
+      case '200':
+        return res
+      case '100004':
+        store.commit('user/SET_LOGOUT')
+        return res
+      default:
+        Toast.clear()
+        Dialog.alert({
+          title: '提示',
+          message: res.message || '服务器异常，请稍后再试'
+        })
+        return Promise.reject(res.message || 'Error')
     }
   },
   error => {
     console.error(error) // for debug
+    Toast.fail('服务器异常\n请稍后再试')
     return Promise.reject(error)
   }
 )
